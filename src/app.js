1945e46f33ac239ef30f9cf0742816c6a10bff89
Zhino Salam <zhinosalam188@gmail.com>
2021-06-06 13:44:30 +0000

    Automatically backed up by Learn

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-class About extends React.Component {
+class App extends React.Component {
   render() {
-    return <h1>This is my about component!</h1>;
+    return <h1>Hello World!</h1>;
   }
 }
 
-export default About; 
+export default App; 
